59e670effcfae6d5e47ab0a0f1342076b35bab49
Matthias GROSPERRIN <matthias.grosperrin@gmail.com>
2015-08-21 12:56:33 +0200

    Add service resolver + default implementation

---
+++ src/MGR.CommandLineParser/ServiceProvider.cs
@@ -0,0 +1,12 @@
+﻿namespace MGR.CommandLineParser
+{
+    public static class ServiceResolver
+    {
+        public static IServiceResolver Current { get; private set; } = new DefaultServiceResolver();
+
+        public static void SetServiceResolver(IServiceResolver serviceResolver)
+        {
+            Current = serviceResolver;
+        }
+    }
+}
