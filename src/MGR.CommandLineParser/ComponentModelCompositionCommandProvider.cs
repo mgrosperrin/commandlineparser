774f21805c253c46cd2bf954ec5c6dacb8b160f8
Matthias GROSPERRIN <matthias.grosperrin@gmail.com>
2015-09-06 13:45:34 +0200

    Use CommandProviderBase in ComponentModelCompositionCommandProvider

--- src/MGR.CommandLineParser/ComponentModelCompositionCommandProvider.cs
+++ src/MGR.CommandLineParser/ComponentModelCompositionCommandProvider.cs
@@ -8,16 +8,9 @@ using MGR.CommandLineParser.Command;
 
 namespace MGR.CommandLineParser
 {
-    public sealed class ComponentModelCompositionCommandProvider : ICommandProvider
+    public sealed class ComponentModelCompositionCommandProvider : CommandProviderBase
     {
-        private readonly Lazy<List<ICommand>> _commands;
-
-        public ComponentModelCompositionCommandProvider()
-        {
-            _commands = new Lazy<List<ICommand>>(BuildCommands);
-        }
-
-        private static List<ICommand> BuildCommands()
+        protected override List<ICommand> BuildCommands()
         {
             var commands = new List<ICommand>();
             using (var catalog = new AggregateCatalog())
@@ -64,21 +57,6 @@ namespace MGR.CommandLineParser
                 }
             }
             return commands;
-        }
-
-        public IEnumerable<ICommand> GetAllCommands() => _commands.Value.OrderBy(command => command.ExtractCommandName()).AsEnumerable();
-
-        public HelpCommand GetHelpCommand(IParserOptions parserOptions, IConsole console) => GetCommand(HelpCommand.Name, parserOptions, console) as HelpCommand;
-
-        public ICommand GetCommand(string commandName, IParserOptions parserOptions, IConsole console)
-        {
-            var command = _commands.Value.FirstOrDefault(c => c.ExtractCommandName().Equals(commandName, StringComparison.OrdinalIgnoreCase));
-            var commandBase = command as CommandBase;
-            if (commandBase != null)
-            {
-                commandBase.Configure(parserOptions, console);
-            }
-            return command;
         }
     }
 }
