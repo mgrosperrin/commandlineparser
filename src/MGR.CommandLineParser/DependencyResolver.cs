2203477397a68885fdf004ae4eb2300a2d271347
ArsenShnurkov <Arsen.Shnurkov@gmail.com>
2016-01-15 10:57:09 +0300

    C# 6.0->5.0 downgrade

--- src/MGR.CommandLineParser/DependencyResolver.cs
+++ src/MGR.CommandLineParser/DependencyResolver.cs
@@ -12,15 +12,18 @@ namespace MGR.CommandLineParser
         /// Gets the current <see cref="IDependencyResolver"/>.
         /// </summary>
         [NotNull]
-        public static IDependencyResolver Current { get; private set; } = DefaultDependencyResolver.Current;
+		public static IDependencyResolver Current { get { return _current; } private set { _current = value; } } 
+		static IDependencyResolver _current = DefaultDependencyResolver.Current;
 
         /// <summary>
         /// Defines the current <see cref="IDependencyResolver"/>.
         /// </summary>
         /// <param name="dependencyResolver">The new current <see cref="IDependencyResolver"/> (cannot be null).</param>
-        public static void SetDependencyResolver([NotNull]IDependencyResolver dependencyResolver)
+		public static void SetDependencyResolver([NotNull]IDependencyResolver dependencyResolver)
         {
-            Guard.NotNull(dependencyResolver, nameof(dependencyResolver));
+			// "nameof" is from C# 6.0 specification
+			// http://stackoverflow.com/questions/31695900/what-is-the-purpose-of-nameof
+            Guard.NotNull(dependencyResolver, "dependencyResolver");
 
             Current = dependencyResolver;
         }
