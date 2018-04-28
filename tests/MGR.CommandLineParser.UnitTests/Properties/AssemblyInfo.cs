b4e3f4794b9b008285cc7d3cd04ff6e65ba4330e
Matthias GROSPERRIN <matthias.grosperrin@gmail.com>
2018-04-28 10:20:21 +0200

    Update projects & build chain to .NET Standard 2.0

--- tests/MGR.CommandLineParser.UnitTests/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("MGR.CommandLineParser.UnitTests")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("24c3db62-07bb-480c-94b6-a9b95d00135b")]
