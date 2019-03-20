afbb65044631c0b2b9be0c5159abbc55ebddc1a1
Matthias GROSPERRIN <mgrosperrin@users.noreply.github.com>
2019-03-20 10:25:23 +0100

    The hosting extensions now extend IHostBuilder & IHost (#89)

--- src/MGR.CommandLineParser.Hosting/Extensions/HostBuilderExtensions.cs
+++ src/MGR.CommandLineParser.Hosting/Extensions/HostBuilderExtensions.cs
@@ -7,29 +7,29 @@ using Microsoft.Extensions.Hosting;
 namespace MGR.CommandLineParser.Hosting.Extensions
 {
     /// <summary>
-    /// Extension's methods for <see cref="HostBuilder"/>.
+    /// Extension's methods for <see cref="IHostBuilder"/>.
     /// </summary>
     public static class HostBuilderExtensions
     {
         /// <summary>
-        /// Configures the <see cref="HostBuilder"/> to run the parser.
+        /// Configures the <see cref="IHostBuilder"/> to run the parser.
         /// </summary>
-        /// <param name="hostBuilder">The <see cref="HostBuilder"/> to configure.</param>
+        /// <param name="hostBuilder">The <see cref="IHostBuilder"/> to configure.</param>
         /// <param name="configureParser">An action to configure the <see cref="IParser"/>.</param>
-        /// <returns>The configured <see cref="HostBuilder"/>.</returns>
-        public static HostBuilder ConfigureParser(this HostBuilder hostBuilder, Action<CommandLineParserBuilder> configureParser)
+        /// <returns>The configured <see cref="IHostBuilder"/>.</returns>
+        public static IHostBuilder ConfigureParser(this IHostBuilder hostBuilder, Action<CommandLineParserBuilder> configureParser)
         {
             hostBuilder.ConfigureParser(configureParser, _ => { });
             return hostBuilder;
         }
         /// <summary>
-        /// Configures the <see cref="HostBuilder"/> to run the parser.
+        /// Configures the <see cref="IHostBuilder"/> to run the parser.
         /// </summary>
-        /// <param name="hostBuilder">The <see cref="HostBuilder"/> to configure.</param>
+        /// <param name="hostBuilder">The <see cref="IHostBuilder"/> to configure.</param>
         /// <param name="configureParser">An action to configure the <see cref="IParser"/>.</param>
         /// <param name="configureParserOptions">An action to configure the <see cref="IParserOptions"/> of the parser.</param>
-        /// <returns>The configured <see cref="HostBuilder"/>.</returns>
-        public static HostBuilder ConfigureParser(this HostBuilder hostBuilder, Action<CommandLineParserBuilder> configureParser, Action<ParserOptionsBuilder> configureParserOptions)
+        /// <returns>The configured <see cref="IHostBuilder"/>.</returns>
+        public static IHostBuilder ConfigureParser(this IHostBuilder hostBuilder, Action<CommandLineParserBuilder> configureParser, Action<ParserOptionsBuilder> configureParserOptions)
         {
             hostBuilder.ConfigureServices(
                 (context, services) =>
@@ -46,22 +46,15 @@ namespace MGR.CommandLineParser.Hosting.Extensions
         /// <summary>
         /// Parse the command line and execute the command if it is valid.
         /// </summary>
-        /// <param name="hostBuilder">The configured <see cref="HostBuilder"/>.</param>
+        /// <param name="hostBuilder">The configured <see cref="IHostBuilder"/>.</param>
         /// <param name="args">The arguments to parse.</param>
         /// <param name="cancellationToken">The token to trigger shutdown.</param>
         /// <returns>A code that represents the result of the parsing and the execution of the command.</returns>
-        public static async Task<int> ParseCommandLineAndExecuteAsync(this HostBuilder hostBuilder, string[] args, CancellationToken cancellationToken = default)
+        public static async Task<int> ParseCommandLineAndExecuteAsync(this IHostBuilder hostBuilder, string[] args, CancellationToken cancellationToken = default)
         {
             var host = hostBuilder.Build();
-            var parserContext = host.Services.GetRequiredService<ParserContext>();
-            parserContext.Arguments = args;
-            await host.RunAsync(cancellationToken);
-            var parsingResult = parserContext.ParsingResult;
-            if (parsingResult.IsValid)
-            {
-                return await parsingResult.ExecuteAsync();
-            }
-            return (int)parserContext.ParsingResult.ParsingResultCode;
+            var executionResult = await host.ParseCommandLineAndExecuteAsync(args, cancellationToken);
+            return executionResult;
         }
     }
 }
