b3ca10f567ee6fb939fa4e708001073815919116
Matthias GROSPERRIN <matthias.grosperrin@gmail.com>
2020-08-21 16:58:10 +0200

    WIP - Add project

---
+++ src/MGR.CommandLineParser.Command.OracleProcedure/OracleProcedureCommandTypeProvider.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Collections.Generic;
+using MGR.CommandLineParser.Extensibility.Command;
+
+namespace MGR.CommandLineParser.Command.OracleProcedure
+{
+    internal class OracleProcedureCommandTypeProvider : ICommandTypeProvider
+    {
+        public IEnumerable<ICommandType> GetAllCommandTypes() => throw new System.NotImplementedException();
+        public ICommandType GetCommandType(string commandName) => throw new System.NotImplementedException();
+    }
+}
---
+++ src/MGR.CommandLineParser.Command.OracleProcedure/OracleSystemDataContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MGR.CommandLineParser.Command.OracleProcedure
+{
+    internal sealed class OracleSystemDataContext : DbContext
+    {
+
+    }
+}
